4deff309a6e73b840f55eeca5cefb7d3d1e5faa0
Hasibul Mallick <108382963+Husseym-max@users.noreply.github.com>
2024-07-24 14:20:03 +0530

    Add files via upload

---
+++ typeConversion.js
@@ -0,0 +1,33 @@
+//Converting to String
+console.log(String(100));//color white that means String
+console.log(100);//color blue that means num
+console.log(String(true));
+console.log(String(null));
+console.log(String(undefined));
+//Tostring
+console.log((100).toString());
+//Converting to Number
+console.log('****NUMBER****');
+console.log(Number('100'));//color blue that means num
+console.log(Number(true));//1
+console.log(Number(null));//0
+console.log(Number(undefined));//NaN
+//CONVERT BY UNARY operator
+console.log('****UNARY****');
+console.log(+'100');//color blue that means num
+//parseInt
+console.log('****parseInt****');
+console.log(parseInt('100'));//100
+//parseFloat
+console.log('****parseFloat****');
+console.log(parseFloat('100.5'));//100.5
+console.log(parseFloat("3.14.15"));
+//Convert to Boolean
+console.log('****Boolean****');
+console.log(Boolean('100'));//true
+console.log(Boolean("false"));
+//exercise
+console.log(String(100));
+let str = toNumber('5');
+console.log(undefined.toString());
+let num = parseInt("42px");
