d11436ca06795a08c51e8cc699da887ed3e02627
Hasibul Mallick <108382963+Husseym-max@users.noreply.github.com>
2024-07-24 14:16:01 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
-// let num = 30;
-
-// if(num < 10){
-//     console.log("num is less than 10");
-// }else{
-//     console.log("num is greater than 10");
-// }
-// //
-// let x =0;
-// if(x++){
-//     console.log("x is greater than 0");
-// }else if(x==0){
-//     console.log("x is equal to 0");
-// }else{
-//     console.log("both the condition are false");
-// }
-let a  ="4";
-let b = "6";
-let result = ++a + b--;
-console.log(result);
-console.log(typeof result);
-var i =11
-var j =--(i++)
-console.log(j)
+let x= '7';
+let y= 7;
+ let res= (x==y) && (x === y);
+ console.log(res);
