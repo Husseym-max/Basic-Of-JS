811542aeaea8b922b323ba008e765766043aa092
Hasibul Mallick <108382963+Husseym-max@users.noreply.github.com>
2024-07-24 14:13:58 +0530

    Add files via upload

---
+++ Opeartor.js
@@ -0,0 +1,15 @@
+// let x = 10;
+// let y = 20;
+// //x++;
+// //x--;
+// //x +=y;
+// console.log(x === y);
+// console.log(x !== y);
+// console.log(x && y);
+// console.log(x === 1 );
+// console.log(x === y);
+// //
+// console.log(0 || "hello"|| true);
+// console.log(y);
+const reactHeading = React.creatElement("h1",{className:first,id:react,children:"HelloReact"});
+ReactDOM.createRoot(document.getElementById("root")).render(reactHeading);
