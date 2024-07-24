9bee11d83d3a32d2111a89fac134830d549200b5
Hasibul Mallick <108382963+Husseym-max@users.noreply.github.com>
2024-07-24 14:20:50 +0530

    Add files via upload

---
+++ typeCoercion.js
@@ -0,0 +1,14 @@
+console.log(5+5);
+let num = '5'+5;
+console.log(typeof num);
+let fullName = "Hasibul" +" "+"Mallick";
+console.log(fullName);
+let sub = '55' - 10;
+console.log(sub);
+console.log(typeof sub); //number
+let x = '5'+5;
+let y= '10'-5;
+let z=x/y;
+console.log(z);
+console.log(typeof z); //number
+console.log(true+true);
